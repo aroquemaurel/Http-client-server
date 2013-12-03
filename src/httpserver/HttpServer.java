fa8602b4917e27ac5437d7b7c6a54588401a6774
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-12-03 22:26:24 +0100

    Refactoring

--- src/httpserver/HttpServer.java
+++ src/httpserver/HttpServer.java
@@ -1,11 +1,15 @@
 package httpserver;
 
+import java.io.File;
 import java.net.*;
 
 class HttpServer
 {
     public static void main(String args[]) throws Exception {
         ServerSocket soc = new ServerSocket(5217);
+        File f = new File("./www-data");
+        f.mkdir(); // Si www-data n'existe pas, on le créé !
+        
         while(true) {
             Socket inSoc = soc.accept();
             HttpRequest request = new HttpRequest(inSoc);
