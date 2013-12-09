a0785131928f50a9e0f9a82709f28367c88d1a6e
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-12-09 21:11:15 +0100

    Ajout head

--- src/httpserver/HttpGetQuery.java
+++ src/httpserver/HttpGetQuery.java
@@ -13,39 +13,9 @@ import java.net.Socket;
  *
  * @author aroquemaurel
  */
-public class HttpGetQuery extends HttpQuery {
+public class HttpGetQuery extends HttpHead {
     public HttpGetQuery(Socket clientConn, String request) throws IOException {
         super(clientConn, request);
-        
-        if(!_file.isFile()) {
-            _file = new HttpFile("www-data/404.html", clientConn.getInputStream());
-        }
-    }
-
-    @Override
-    protected String getStatusLine() {
-        String status;
-        if(!_file.isFile()) {
-            status = "HTTP/1.0 404 File not found\r\n";
-        } else if(!_file.permssions()) {
-            status = "HTTP/1.0 403 Forbidden\r\n";
-        } else {
-            status = "HTTP/1.0 200 OK\r\n";
-        }
-
-        return status;
-    }
-
-    @Override
-    protected String getQuery() throws Exception {
-        String ret = getStatusLine() + _serverLine + getContentTypeLine() + getContentLengthLine();
-        
-        return ret;
-    }
-
-    @Override
-    protected String getContentTypeLine() {
-        return "Content-type: text/html\r\n"; // TODO
     }
 
     @Override
